283912bad94a4879a9998adefbb41fefaee3da08
nmluthfi <nurmuhammadluthfi@gmail.com>
2023-11-15 18:17:20 +0700

    add time calc. and did 5 experiments

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import argparse
+import time
 import numpy as np
 from data_loader import load_data
 from train import train
@@ -20,16 +21,15 @@ np.random.seed(555)
 # parser.add_argument('--using_all_hops', type=bool, default=True,
 #                     help='whether using outputs of all hops or just the last hop when making prediction')
 
-
-# default settings for Book-Crossing
+# default settings for Crypto
 parser = argparse.ArgumentParser()
-parser.add_argument('--dataset', type=str, default='book', help='which dataset to use')
-parser.add_argument('--dim', type=int, default=4, help='dimension of entity and relation embeddings')
+parser.add_argument('--dataset', type=str, default='crypto', help='which dataset to use')
+parser.add_argument('--dim', type=int, default=32, help='dimension of entity and relation embeddings')
 parser.add_argument('--n_hop', type=int, default=2, help='maximum hops')
-parser.add_argument('--kge_weight', type=float, default=1e-2, help='weight of the KGE term')
+parser.add_argument('--kge_weight', type=float, default=1e-3, help='weight of the KGE term')
 parser.add_argument('--l2_weight', type=float, default=1e-5, help='weight of the l2 regularization term')
-parser.add_argument('--lr', type=float, default=1e-3, help='learning rate')
-parser.add_argument('--batch_size', type=int, default=1024, help='batch size')
+parser.add_argument('--lr', type=float, default=1e-2, help='learning rate')
+parser.add_argument('--batch_size', type=int, default=128, help='batch size')
 parser.add_argument('--n_epoch', type=int, default=10, help='the number of epochs')
 parser.add_argument('--n_memory', type=int, default=32, help='size of ripple set for each hop')
 parser.add_argument('--item_update_mode', type=str, default='plus_transform',
@@ -37,9 +37,28 @@ parser.add_argument('--item_update_mode', type=str, default='plus_transform',
 parser.add_argument('--using_all_hops', type=bool, default=True,
                     help='whether using outputs of all hops or just the last hop when making prediction')
 
+# default settings for Book-Crossing
+# parser = argparse.ArgumentParser()
+# parser.add_argument('--dataset', type=str, default='book', help='which dataset to use')
+# parser.add_argument('--dim', type=int, default=4, help='dimension of entity and relation embeddings')
+# parser.add_argument('--n_hop', type=int, default=2, help='maximum hops')
+# parser.add_argument('--kge_weight', type=float, default=1e-2, help='weight of the KGE term')
+# parser.add_argument('--l2_weight', type=float, default=1e-5, help='weight of the l2 regularization term')
+# parser.add_argument('--lr', type=float, default=1e-3, help='learning rate')
+# parser.add_argument('--batch_size', type=int, default=1024, help='batch size')
+# parser.add_argument('--n_epoch', type=int, default=10, help='the number of epochs')
+# parser.add_argument('--n_memory', type=int, default=32, help='size of ripple set for each hop')
+# parser.add_argument('--item_update_mode', type=str, default='plus_transform',
+#                     help='how to update item at the end of each hop')
+# parser.add_argument('--using_all_hops', type=bool, default=True,
+#                     help='whether using outputs of all hops or just the last hop when making prediction')
 
+start_time = time.time()
 args = parser.parse_args()
 
 show_loss = False
 data_info = load_data(args)
 train(args, data_info, show_loss)
+end_time = time.time()
+total_time = end_time - start_time
+print(f'Total run time: {total_time} seconds')
